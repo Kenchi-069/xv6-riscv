467f6e1f8359cf047fb3960d79f3310980f460fa
Kenchi <prasoonbadjatya689@gmail.com>
2026-02-21 05:26:27 +0530

    hello again

--- kernel/shm.c
+++ kernel/shm.c
@@ -26,17 +26,21 @@ void shm_init_system(void)
 uint64
 sys_shm_init(void)
 {
-  if (shm_table.page != 0)
+  acquire(&shm_table.lock);
+  if (shm_table.refcnt >= 0)
   {
-    return 0;
+    release(&shm_table.lock);
+    return -1; // Already initialized
   }
   shm_table.page = kalloc();
   if (shm_table.page == 0)
   {
+    release(&shm_table.lock);
     return -1;
   }
   memset(shm_table.page, 0, PGSIZE);
   shm_table.refcnt = 0;
+  release(&shm_table.lock);
   return 0;
 }
 
@@ -46,8 +50,10 @@ sys_shm_attach(void)
   struct proc *p = myproc();
   uint64 pa;
 
+  acquire(&shm_table.lock);
   if (shm_table.page == 0)
   {
+    release(&shm_table.lock);
     return -1;
   }
 
@@ -55,50 +61,72 @@ sys_shm_attach(void)
   pte_t *pte = walk(p->pagetable, SHMEM, 0);
   if (pte && (*pte & PTE_V))
   {
-    return -1;
+    release(&shm_table.lock);
+    return -1; // Already attached
   }
 
   if (mappages(p->pagetable, SHMEM, PGSIZE, pa, PTE_R | PTE_W | PTE_U) < 0)
   {
+    release(&shm_table.lock);
     return -1;
   }
 
   shm_table.refcnt++;
+  release(&shm_table.lock);
 
-  return SHMEM;
+  return SHMEM; // Return virtual address
 }
 
 uint64
 sys_shm_detach(void)
 {
   struct proc *p = myproc();
+
+  acquire(&shm_table.lock);
+
+  // Refcount decrement is tricky if we don't know if this process actually attached it.
+  // But lab instructions imply simple refcount handling.
+  // Typically detach unmaps.
+
   pte_t *pte = walk(p->pagetable, SHMEM, 0);
   if (pte == 0 || (*pte & PTE_V) == 0)
   {
-    return -1;
+    release(&shm_table.lock);
+    return -1; // Not mapped
   }
+
+  // uvmunmap(pagetable, va, npages, do_free)
+  // do_free should be 0 because the physical page is managed by shm_table
   uvmunmap(p->pagetable, SHMEM, 1, 0);
 
   if (shm_table.refcnt > 0)
     shm_table.refcnt--;
+
+  release(&shm_table.lock);
   return 0;
 }
 
 uint64
 sys_shm_destroy(void)
 {
+  acquire(&shm_table.lock);
   if (shm_table.page == 0)
   {
+    release(&shm_table.lock);
     return -1;
   }
 
   if (shm_table.refcnt > 0)
   {
-    return -1;
+    release(&shm_table.lock);
+    return -1; // Can't destroy if in use
   }
+
   kfree(shm_table.page);
   shm_table.page = 0;
   shm_table.refcnt = 0;
+
+  release(&shm_table.lock);
   return 0;
 }
 
@@ -106,6 +134,8 @@ uint64
 sys_shm_refcnt(void)
 {
   int cnt;
+  acquire(&shm_table.lock);
   cnt = shm_table.refcnt;
+  release(&shm_table.lock);
   return cnt;
 }
